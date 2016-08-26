981b979b33b19e416b656d2b5982b48af0153474
capacitypp <aoi0x4c4f5645@gmail.com>
2016-08-26 17:18:21 +0900

    [add] add php-5.0.5

---
+++ php/make-php-5.0.5/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source ../../environment.sh
+echo "prefix : "$PREFIX
+
+rm -r -f $BUILDDIRPATH
+mkdir $BUILDDIRPATH
+cd $BUILDDIRPATH
+
+cp -R $SRCDIRPATH .
+patch -u -p0 < $DIRPATH/$SRCDIRNAME.patch
+
+cd $SRCDIRNAME
+
+PATH=$PATH:/usr/local/flex/flex-2.5.4/bin
+export PATH
+
+#./configure --prefix=$PREFIX 2>&1 --with-apxs=/usr/local/apache/apache-1.3.42/bin/apxs --with-gd=/usr/local/libgd/libgd-2.2.3 --with-png-dir=/usr/local/libpng/libpng-1.6.24 --with-zlib-dir=/usr/local/zlib/zlib-1.2.8 --with-pgsql=/usr/local/postgresql/postgresql-7.3 --with-libxml-dir=/usr/local/libxml2/libxml2-2.5.10 | tee $DIRPATH/configure.txt | grep -i error
+./configure --prefix=$PREFIX 2>&1 --with-apxs=/usr/local/apache/apache-1.3.42/bin/apxs --with-pgsql=/usr/local/postgresql/postgresql-7.3 --with-libxml-dir=/usr/local/libxml2/libxml2-2.5.10 | tee $DIRPATH/configure.txt | grep -i error
+

7940954f3273f647264bbcd2ffcfa6c91b040852
capacitypp <aoi0x4c4f5645@gmail.com>
2016-09-03 05:20:30 +0900

    [update] update php-7.0.10

--- php/make-php-7.0.10/configure.sh
+++ php/make-php-7.0.10/configure.sh
@@ -17,6 +17,6 @@ export PATH
 export LD_LIBRARY_PATH=/usr/local/readline/readline-4.3/lib
 
 #./configure --prefix=$PREFIX 2>&1 --with-apxs=/usr/local/apache/apache-1.3.42/bin/apxs --with-gd=/usr/local/libgd/libgd-2.2.3 --with-png-dir=/usr/local/libpng/libpng-1.6.24 --with-zlib-dir=/usr/local/zlib/zlib-1.2.8 --with-pgsql=/usr/local/postgresql/postgresql-7.3 --with-libxml-dir=/usr/local/libxml2/libxml2-2.6.20 | tee $DIRPATH/configure.txt | grep -i error
-./configure --prefix=$PREFIX 2>&1 --with-apxs2=/usr/local/httpd/httpd-2.4.23/bin/apxs --with-gd --with-png-dir=/usr/local/libpng/libpng-1.6.24 --with-zlib-dir=/usr/local/zlib/zlib-1.2.8 --with-pdo-pgsql=/usr/local/postgresql/postgresql-9.5.4 --with-pgsql=/usr/local/postgresql/postgresql-9.5.4 --with-libxml-dir=/usr/local/libxml2/libxml2-2.6.20 | tee $DIRPATH/configure.txt | grep -i error
+./configure --prefix=$PREFIX 2>&1 --with-apxs2=/usr/local/httpd/httpd-2.4.23/bin/apxs --with-gd --with-png-dir=/usr/local/libpng/libpng-1.6.24 --with-zlib-dir=/usr/local/zlib/zlib-1.2.8 --with-pdo-pgsql=/usr/local/postgresql/postgresql-9.5.4 --with-pgsql=/usr/local/postgresql/postgresql-9.5.4 --with-libxml-dir=/usr/local/libxml2/libxml2-2.9.4 --with-pdo-mysql --enable-mbstring --enable-zip --with-curl=/usr/local/curl/curl-7.50.1 | tee $DIRPATH/configure.txt | grep -i error
 #./configure --prefix=$PREFIX 2>&1 --with-apxs=/usr/local/apache/apache-1.3.42/bin/apxs --with-pgsql=/usr/local/postgresql/postgresql-7.3 --with-libxml-dir=/usr/local/libxml2/libxml2-2.6.20 | tee $DIRPATH/configure.txt | grep -i error
 
